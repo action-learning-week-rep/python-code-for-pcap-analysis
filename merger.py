a7ee91bce504470f216c06617bf81d9902fc9fa0
SANUJAN420 <124000105+SANUJAN420@users.noreply.github.com>
2023-02-02 13:33:27 +0100

    Update merger.py

--- merger.py
+++ merger.py
@@ -17,6 +17,13 @@ def push_time_avg(array):
         pass
 
 
+    # The longest push time in the stream
+
+        # This function long_push_time(array) calculates the longest time between consecutive elements in an input array of time stamps array.
+        # It does this by subtracting each consecutive element (i+1) with the previous one (i) and appending the difference to a list push.
+        # The function then returns the maximum value from the push list as the longest time between consecutive elements.
+        # If the push list is empty, the function returns None.
+    
 def long_push_time(array):
     push = []
     for i in range(len(array) - 1):
@@ -27,6 +34,10 @@ def long_push_time(array):
     except:
         pass
 
+    
+# The function total_pushtime(array) takes an input array and calculates the total push time between consecutive elements in the array.
+# It adds up the difference between each element and the next one using a loop, and returns the sum of these differences as the total push time.
+# The try-except block is used to catch any exceptions that might occur during the calculation and return None if an exception is caught.
 
 def total_pushtime(array):
     time = 0
@@ -40,16 +51,16 @@ def total_pushtime(array):
 
 
 # ---------------------------calculating number of tcp stream per client and there size for traffic-------------------------------
-list_of_files = []
+list_of_files = [] # Initialize an empty list to store the names of files in the specified directory.
 for file in os.listdir(f"./{dirctory}"):
-    list_of_files.append(file)
-list_all_streams = []
-print("all streams have been read")
+    list_of_files.append(file)  # Loop through the list of files in the specified directory (dirctory).
+list_all_streams = [] # Initialize an empty list to store the results of processing each file.
+print("all streams have been read") # Print a message indicating that all streams have been read.
 for onefile in list_of_files:
     streams = rdpcap(f"./{dirctory}/{onefile}")
     start_time = streams[0].time
     end_time = streams[-1].time
-    total_stream_time = end_time - start_time
+    total_stream_time = end_time - start_time # Calculate the total time of the streams by subtracting the start time from the end time and store the result in the total_stream_time variable.
     number_of_packets = len(streams)
     number_flags_push = 0
     list_push_times = []
@@ -65,6 +76,16 @@ for onefile in list_of_files:
     list_all_streams.append(resualts)
 print("first phase is done")
 print(len(list_all_streams))
+
+
+# 1.Reads a list of files in the specified directory (diretory).
+# 2.Creates a list of packets by reading the contents of each file using the pyshark library.
+# 3.Calculates the total TCP load (sum of packet lengths) for each file and adds it to the list_tcp_len.
+# 4.Adds the total TCP load as an attribute to each item in the list_all_streams.
+# 5.Filters the list_all_streams by removing the items with "push_flags" equal to 0.
+# 6.Assigns the filtered list to the variable "detected".
+# 7.Prints messages indicating that the second phase, filtration, and detection are done
+
 
 list_of_packets = []
 for file in list_of_files:
@@ -131,6 +152,11 @@ for i in list_all_streams:
         list_filtered_list.append(i)
 print("filtration is done")
 
+
+# This code opens a new .csv file in write mode with the name dirctory+.csv using the open function and file handle
+# filecsv. Then, it gets the keys of the first dictionary in the detected list as the fieldnames for the csv file. It
+# uses the csv module's DictWriter class to write the fieldnames as the header of the csv file. Finally, it iterates
+# through each row in the detected list, writing each dictionary in the list as a row in the csv file.
 detected = list_filtered_list
 with open(f"{dirctory}+.csv", "w") as filecsv:
     filednames = detected[0].keys()
